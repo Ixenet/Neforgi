5386d4be7c6d907655ccc57a671eeb3bc015e75a
Martynoff <martynoff.work@gmail.com>
2025-03-25 12:42:38 +0300

    Changed items naming

--- src/assets/scripts/dataBaseEmulator.js
+++ src/assets/scripts/dataBaseEmulator.js
@@ -11,7 +11,7 @@ class ClothingData {
     constructor() {
         this.items = [];
         this.addItem(1, "Nike Cortez", 90, "./assets/images/Sneakers/6f57ac9d331789699f9e341c89601121.jpg");
-        this.addItem(2, "Штаны нефорские y2k", 200, "./assets/images/Pants/image.webp");
+        this.addItem(2, "Штаны y2k", 200, "./assets/images/Pants/image.webp");
         this.addItem(3, "Цепочка Chrome Hearts", 6000, "./assets/images/Accessories/item-8550995e-b5a4-40f1-af24-5b98f494f3d6.jpg.webp");
         this.addItem(4, "Свитшот Marselin", 60, "./assets/images/Sweatshirt/86879511.png");
         this.addItem(5, "Цепочка Chrome Hearts", 1500, "./assets/images/Accessories/item-fd4665a6-0e4a-4629-9b95-f37933b7bf01.jpg.webp");
